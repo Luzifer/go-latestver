748bdf300775070c336662c5fe71c4ba74916d80
Knut Ahlers <knut@ahlers.me>
2025-07-12 01:18:29 +0200

    ci: update chart version to tag version
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- ci/patch-chart-version.sh
+++ ci/patch-chart-version.sh
@@ -5,12 +5,9 @@ chart_yaml=charts/latestver/Chart.yaml
 
 # Patch latest App-Version into Chart
 yq -iP ".appVersion = \"v${TAG_VERSION}\"" ${chart_yaml}
+yq -iP ".version = \"${TAG_VERSION}\"" ${chart_yaml}
 
-# Validate there has been a change before patching the chart version
+# Validate there has been a change before adding
 git diff --exit-code -- ${chart_yaml} >/dev/null && exit 0 || true
-
-# There were changes, we need to patch the chart version
-chart_ver=$(yq '.version' ${chart_yaml})
-yq -iP ".version = \"$(semver -i minor ${chart_ver})\"" ${chart_yaml}
 
 git add ${chart_yaml}
